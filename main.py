3bb686f915faf702de57ca0ff4aed300f05236c8
Mara <mara-li@outlook.fr>
2021-01-27 15:19:35 +0100

    debug & continue

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import discord
 from discord.ext import commands
+from discord.utils import get
+from discord import CategoryChannel
 import os
 import sqlite3
 
@@ -21,34 +23,39 @@ async def clear(ctx, amount=3):
     await ctx.channel.purge(limit=amount)
 
 @bot.command()
-async def appartement(ctx):
+async def config_appart(ctx):
+    await ctx.message.delete()
     def checkRep(message):
         return message.author == ctx.message.author and ctx.message.channel == message.channel
     db = sqlite3.connect("owlly.db")
     c = db.cursor()
-    data=c.fetchall()
     question = await ctx.send (f"Quel est le titre de l'embed ?")
     titre = await bot.wait_for("message", timeout = 300, check = checkRep)
-    titre = titre.content
+    typeM = titre.content
+    await question.delete()
     question = await ctx.send (f"Quelle est sa description ?")
     desc = await bot.wait_for("message", timeout=300, check=checkRep)
-    desc = desc.content
+    await question.delete()
     question = await ctx.send (f"Quelle couleur voulez vous utiliser ?")
-    col = await bot.wait_for("message", timeout=300, check=checkRep)
-    col = col.content
+    color = await bot.wait_for("message", timeout=300, check=checkRep)
+    col = color.content
     if (col.find ("#") == -1):
         await ctx.send (f"Erreur ! Vous avez oublié le # !")
         return
     else:
+        await question.delete()
         col = col.replace("#", "0x")
         col = int(col, 16)
-    embed = discord.Embed(title=titre, description=desc, color=col)
+    embed = discord.Embed(title=titre.content, description=desc.content, color=col)
     react = await ctx.send(embed=embed)
     await react.add_reaction("🏠")
-    sql= ("INSERT INTO config (type, chan, mid) VALUES (?,?,?)")
+    sql= ("INSERT INTO CONFIG (type, chan, mid) VALUES (?,?,?)")
     mid= react.id
     chan = react.channel.id
-    var=(titre, chan, mid)
+    await desc.delete()
+    await titre.delete()
+    await color.delete()
+    var=(typeM, chan, mid)
     print(var)
     c.execute(sql, var)
     db.commit()
@@ -56,11 +63,88 @@ async def appartement(ctx):
     db.close()
 
 @bot.command()
-async def dbtest(ctx):
+async def config_category(ctx):
+    emoji = ["1️⃣", "2️⃣", "3️⃣", "4️⃣", "5️⃣", "6️⃣", "7️⃣", "8️⃣", "9️⃣"]
+    def checkEmoji(reaction, user):
+        return ctx.message.author == user and message.id == reaction.message.id and (str(reaction) in emote)
+    def checkRep(message):
+        return message.author == ctx.message.author and ctx.message.channel == message.channel
     db = sqlite3.connect("owlly.db")
     c = db.cursor()
-    c.execute("SELECT * FROM config")
-    data = c.fetchall()
-    for x in data:
-        print(x)
+    chan = []
+    question = await ctx.send("Merci d'envoyer l'ID des catégories que vous souhaitez utiliser pour cette configuration.")
+    while True:
+        channels = await bot.wait_for("message", timeout=300, check = checkRep)
+        if channels.content.lower() == 'stop':
+            break
+        elif channels.content.lower() == 'cancel':
+            return
+        chan.append(channels.content)
+        await channels.delete()
+    if len(chan) >= 10 :
+        await ctx.send ("Erreur ! Vous ne pouvez pas mettre plus de 9 catégories !")
+        return
+    namelist=[]
+    for i in range(0,len(chan)):
+        number=int(chan[i])
+        guild= ctx.message.guild
+        cat = get(guild.categories, id=number)
+        print (cat)
+        phrase = f"{emoji[i]} : {cat}"
+        namelist.append(phrase)
+    msg = "\n".join(namelist)
+    await ctx.send (f"Votre message sera donc envoyé dans le ou les canaux suivants :\n {msg}")
+    await ctx.send (f"Quel est le titre de l'embed ?")
+    titre = await bot.wait_for("message", timeout = 300, check = checkRep)
+    question = await ctx.send (f"Quelle couleur voulez vous utiliser ?")
+    color = await bot.wait_for("message", timeout=300, check=checkRep)
+    col = color.content
+    if (col.find ("#") == -1):
+        await ctx.send (f"Erreur ! Vous avez oublié le # !")
+        return
+    else:
+        await question.delete()
+        col = col.replace("#", "0x")
+        col = int(col, 16)
+    embed = discord.Embed(title=titre.content, description=msg, color=col)
+    react = await ctx.send(embed=embed)
+    for i in range(0,len(chan)):
+        await react.add_reaction(emoji[i])
+    titre = titre.content
+    sql = ("INSERT INTO CHANNEL (titre, category, channel, mid) VALUES (?,?,?,?)")
+    mid= react.id
+    creat = react.channel.id
+    var = (titre, chan, creat, mid)
+    c.execute(sql, var)
+    db.commit()
+    c.close()
+    db.close()
+
+@bot.command()
+async def msg(ctx):
+    mid = 803968284072214548
+    chan = bot.get_channel(803737008450043934)
+    message = await chan.fetch_message(803968284072214548)
+    reaction = message.content
+    print(reaction)
+
+@bot.event
+async def on_reaction_add (ctx, reaction, user):
+    db = sqlite3.connect("owlly.db")
+    c = db.cursor()
+    c.execute("SELECT * FROM CONFIG")
+    appart = c.fetchall()
+    appartDict={}
+    for i in range(0, (len(appart))):
+        extra = {appart[i][0]:(appart[i][1], appart[i][2])}
+        appartDict.update(extra)
+    # On a donc un dictionnaire avec les channels d'appartement (de toute façon à la fin y'aura probablement qu'un seul). On va faire pareil, mais cette fois pour les bâtiments. 
+    c.execute("SELECT * FROM CHANNEL")
+    room = c.fetchall()
+    roomDict = {}
+    for i in range(0, (len(room))):
+        extra={room[i][0]:(room[i][1],room[i][2], room[i][3], room[i][4])}
+        roomDict.update(extra)
+    
+
 bot.run(token)
