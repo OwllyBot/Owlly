c8e2d6e4cc9af819ad2f99a0daabfbdc695999b7
Lisandra <mara-li@outlook.fr>
2021-03-16 16:27:33 +0100

    list view

--- cogs/Menu_fonction/list_creator.py
+++ cogs/Menu_fonction/list_creator.py
@@ -39,4 +39,27 @@ def list_category(ctx):
     db = sqlite3.connect("owlly.db", timeout=3000)
     c = db.cursor()
     idS=ctx.guild.id
+    SQL="SELECT * FROM CATEGORY WHERE idS=?"
+    c.execute(SQL,(idS,))
+    info=c.fetchall()
+    msg=""
+    if len(info) != 0:
+        for i in info:
+            msg = f"⭐ {i[0]} dans <#{i[1]}> :\n"
+            cat_list=i[2].split(",")
+            msg_cat=[]
+            for i in cat_list:
+                cat_name=get(ctx.guild.categories, id=int(i))
+                msg_cat.append(cat_name)
+            msg_cat_str="       \n🔹".join(msg_cat)
+            msg=msg+f"\n    ◽ Catégories :\n{msg_cat_str}"
+            config=i[4]
+            if config==1:
+                para = "    ◽ Nom : Libre"
+            else:
+                para = "    ◽ Nom : Nom du personnage"
+        msg=msg+"\n\n"
+
+
+
     
