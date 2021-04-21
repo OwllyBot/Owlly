69c945a5f42918edf30033ae32edd7e76bfb504e
Mara <mara-li@outlook.fr>
2021-04-21 17:35:27 +0200

    ??? wtf

--- cogs/member.py
+++ cogs/member.py
@@ -83,6 +83,7 @@ class memberUtils(commands.Cog, name="Membre", description="Des commandes géran
 					l = l.replace("*", "")
 					physique_msg=physique_msg+f"**__{l.capitalize()}__** : {m}\n"
 			msg = general_msg+"\n"+physique_msg+"\n"+f"⋆⋅⋅⋅⊱∘──────∘⊰⋅⋅⋅⋆\n *Joueur* : {member.mention}"
+			print(msg)
 		return msg, img
 
 	async def validation(self, ctx, msg, img, chartype, member: discord.Member):
