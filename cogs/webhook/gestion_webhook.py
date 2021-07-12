273ebe35340c8ff3a71c4cc79d894015cb845035
mara-li <mara-li@outlook.fr>
2021-07-12 16:46:22 +0200

    [OwllyBot/OwllyDocs#2] Finalize menu

--- cogs/webhook/gestion_webhook.py
+++ cogs/webhook/gestion_webhook.py
@@ -42,19 +42,21 @@ def number_check(ctx):
     else:
         return "inactive"
 
+
 def name_check(ctx, name):
     db = sqlite3.connect("src/owlly.db", timeout=3000)
     c = db.cursor()
-    sql="SELECT * FROM DC WHERE idS=? AND idU=? AND Nom=?"
-    var=(ctx.guild.id, ctx.message.author.id, name)
+    sql = "SELECT * FROM DC WHERE idS=? AND idU=? AND Nom=?"
+    var = (ctx.guild.id, ctx.message.author.id, name)
     c.execute(sql, var)
-    check=c.fetchone()
+    check = c.fetchall()
     c.close()
     db.close()
-    if check is None:
+    if check is None or len(check) == 0:
         return "ok"
     else:
         return "error"
+
 
 def name_persona(ctx, nom, id_Persona):
     db = sqlite3.connect("src/owlly.db", timeout=3000)
@@ -145,10 +147,25 @@ async def token_Persona(ctx, bot):
         token = "^/" + token + "$"
         return token
 
-def check_token (ctx, token):
+
+def check_token(ctx, token):
     db = sqlite3.connect("src/owlly.db", timeout=3000)
     c = db.cursor()
-    
+    sql = "SELECT idDC FROM DC WHERE (token=? AND idU = ? AND idS = ?)"
+    var = (
+        token,
+        ctx.message.author.id,
+        ctx.guild.id,
+    )
+    c.execute(sql, var)
+    check = c.fetchall()
+    c.close()
+    db.close()
+    if check is None or len(check) == 0:
+        return "ok"
+    else:
+        return "error"
+
 
 def search_Persona(ctx, nom):
     db = sqlite3.connect("src/owlly.db", timeout=3000)
@@ -174,7 +191,7 @@ def search_Persona(ctx, nom):
             ),
         )
         perso = c.fetchone()
-        if perso is None:
+        if perso is None or len(perso) == 0:
             return "error"
         else:
             return perso[0]
@@ -182,18 +199,73 @@ def search_Persona(ctx, nom):
         return perso[0]
 
 
-
 async def webhook_delete(ctx, bot):
     db = sqlite3.connect("src/owlly.db", timeout=3000)
     c = db.cursor()
-    def checkRep(message):  
-        return message.author == ctx.message.author and ctx.message.channel == message.channel
-    q=await ctx.send("Quel est le Persona que vous souhaitez supprimer ? Vous pouvez utiliser son nom ou son token.")
-    rep= await bot.wait_for("message", timeout=300, check=checkRep)
-    if rep.content.lower() == "stop" or rep.content.lower() == "cancel":   
-        await ctx.send("Annulation !", delete_after=30)   
-        await q.delete()   
-        await rep.delete()   
+
+    def checkRep(message):
+        return (
+            message.author == ctx.message.author
+            and ctx.message.channel == message.channel
+        )
+
+    q = await ctx.send(
+        "Quel est le Persona que vous souhaitez supprimer ? Vous pouvez utiliser son nom ou son token."
+    )
+    rep = await bot.wait_for("message", timeout=300, check=checkRep)
+    if rep.content.lower() == "stop" or rep.content.lower() == "cancel":
+        await ctx.send("Annulation !", delete_after=30)
+        await q.delete()
+        await rep.delete()
         return
-    persona=rep.content
-    
+    persona = rep.content
+    # Check if token :
+    sql = "SELECT token FROM DC WHERE (idS = ? AND idU = ? AND token = ?)"
+    var = (
+        ctx.guild.id,
+        ctx.message.author.id,
+        persona,
+    )
+    c.execute(sql, var)
+    check = c.fetchall()
+    config = ""
+    if check is None or len(check) == 0:
+        # Check nom
+        sql = "SELECT Nom FROM DC WHERE (idS= ? AND idU = ? AND Nom = ?)"
+        var = (
+            ctx.guild.id,
+            ctx.message.author.id,
+            persona,
+        )
+        c.execute(sql, var)
+        check = c.fetchall()
+        if check is None or len(check) == 0:
+            await ctx.send(
+                "Ce personnage n'existe pas, merci de vérifier votre saisie."
+            )
+            return
+        else:
+            config = "Nom"
+    else:
+        config = "token"
+    if config == "Nom":
+        sql = "DELETE FROM DC WHERE (idS=? and idU=? AND Nom=?)"
+        var = (
+            ctx.guild.id,
+            ctx.message.author.id,
+            persona,
+        )
+        c.execute(sql, var)
+    elif config == "token":
+        sql = "DELETE FROM DC WHERE (idS = ? AND idU = ? AND token = ?)"
+        var = (
+            ctx.guild.id,
+            ctx.message.author.id,
+            persona,
+        )
+        c.execute(sql, var)
+    await ctx.send("La suppression a bien été effectuée !")
+    db.commit()
+    c.close()
+    db.close()
+    return
--- cogs/webhook/menu_webhook.py
+++ cogs/webhook/menu_webhook.py
@@ -4,6 +4,7 @@ import sqlite3
 from discord.ext import commands
 import os
 import uuid
+from discord.colour import Color
 
 
 async def webhook_create(ctx, bot):
@@ -49,6 +50,12 @@ async def webhook_create(ctx, bot):
         if image == "stop":
             return
         token = await gestion.token_Persona(ctx, bot)
+        check_token = gestion.check_token(ctx, bot, token)
+        if check_token == "error":
+            await ctx.send("Erreur ! Ce token est déjà pris.")
+            await q.delete()
+            await rep.delete()
+            return
         if token == "stop":
             return
         sql = "INSERT INTO DC (idDC, idS, idU, Nom, Avatar, Token, active) VALUES (?,?,?,?,?,?,?)"
@@ -66,6 +73,7 @@ async def webhook_create(ctx, bot):
         db.commit()
         c.close()
         db.close()
+
 
 async def webhook_edit(ctx, bot, idDC, config):
     def checkRep(message):
@@ -86,7 +94,7 @@ async def webhook_edit(ctx, bot, idDC, config):
             await rep.delete()
             return
         name = gestion.name_persona(ctx, rep.content, idDC)
-        check=gestion.name_check(ctx, name)
+        check = gestion.name_check(ctx, name)
         if check == "error":
             await ctx.send("Erreur ! Ce nom est déjà pris.")
             await q.delete()
@@ -123,6 +131,10 @@ async def webhook_edit(ctx, bot, idDC, config):
 
     elif config == "3":  # Token
         token = gestion.token_Persona(ctx, bot)
+        check = gestion.check_token(ctx, token)
+        if check == "error":
+            await ctx.send("Erreur ! Ce token est déjà pris.")
+            return
         if token == "stop":
             return
         sql = "UPDATE DC SET token=? WHERE idDC = ?"
@@ -132,4 +144,136 @@ async def webhook_edit(ctx, bot, idDC, config):
         c.close()
         db.close()
         await ctx.send("Modification enregistrée !")
+        return
+
+async def menu_edit(ctx,bot):
+    emoji = ["1️⃣", "2️⃣", "3️⃣", "❌", "✅"]
+    def checkRep(message):  
+        return message.author == ctx.message.author and ctx.message.channel == message.channel
+    def checkValid(reaction, user):  
+        return (ctx.message.author == user and q.id == reaction.message.id and str(reaction.emoji) in emoji)
+    edition=discord.Embed(title="PERSONAE - ÉDITION", color=Color.dark_teal())
+    edition.add_field(
+        name="1️⃣ | Nom",
+        value="Édition du nom",
+        inline=False,
+    )
+    edition.add_field(
+        name="2️⃣ | Avatar",
+        value="Édition de l'avatar",
+        inline=False
+    )
+    edition.add_field(
+        name="3️⃣ | Token",
+        value="Édition du token",
+        inline=False
+    )
+    edition.set_footer(
+        text="Cliquez sur la réaction pour choisir !\n ❌ Pour quitter le menu.")
+    q=await ctx.send(embed=edition)
+    await q.add_reaction("1️⃣")
+    await q.add_reaction("2️⃣")
+    await q.add_reaction("3️⃣")
+    await q.add_reaction("❌")
+    reaction, user = await bot.wait_for("reaction_add", timeout=300, check=checkValid)
+    if reaction.emoji == "1️⃣":
+        await q.delete()
+        q = await ctx.send("Merci d'envoyer le nom ou le token du personnage que vous voulez éditer.")
+        rep = await bot.wait_for("message", timeout=300, check=checkRep)
+        persona = rep.content
+        if rep.content.lower() == "stop" or rep.content.lower() == "cancel":
+            await ctx.send("Annulation !", delete_after=30)
+            await q.delete()
+            await rep.delete()
+            return
+        idDC = gestion.search_Persona(ctx, persona)
+        if idDC == "error":
+            await ctx.send("Erreur ! Ce Persona n'existe pas.")
+            return
+        await webhook_edit(ctx, bot, idDC, "1")
+        return
+    elif reaction.emoji == "2️⃣":
+        await q.delete()
+        q = await ctx.send("Merci d'envoyer le nom ou le token du personnage que vous voulez éditer.")
+        rep = await bot.wait_for("message", timeout=300, check=checkRep)
+        persona = rep.content
+        if rep.content.lower() == "stop" or rep.content.lower() == "cancel":
+            await ctx.send("Annulation !", delete_after=30)
+            await q.delete()
+            await rep.delete()
+            return
+        idDC = gestion.search_Persona(ctx, persona)
+        if idDC == "error":
+            await ctx.send("Erreur ! Ce Persona n'existe pas.")
+            return
+        await webhook_edit(ctx, bot, idDC, "2")
+        return
+    elif reaction.emoji == "3️⃣":
+        await q.delete()
+        q = await ctx.send("Merci d'envoyer le nom ou le token du personnage que vous voulez éditer.")
+        rep = await bot.wait_for("message", timeout=300, check=checkRep)
+        persona = rep.content
+        if rep.content.lower() == "stop" or rep.content.lower() == "cancel":
+            await ctx.send("Annulation !", delete_after=30)
+            await q.delete()
+            await rep.delete()
+            return
+        idDC = gestion.search_Persona(ctx, persona)
+        if idDC == "error":
+            await ctx.send("Erreur ! Ce Persona n'existe pas.")
+            return
+        await webhook_edit(ctx, bot, idDC, "3")
+        return
+    else:
+        await q.delete()
+        await ctx.send("Annulation !")
+        return
+    
+async def menu(ctx, bot):
+    emoji = ["1️⃣","2️⃣", "3️⃣", "❌", "✅"]
+    def checkRep(message):  
+        return message.author == ctx.message.author and ctx.message.channel == message.channel
+    def checkValid(reaction, user):  
+        return (
+          ctx.message.author == user and q.id == reaction.message.id and str(reaction.emoji) in emoji)
+    embed = discord.Embed(title="GESTION PERSONAE", color=Color.dark_teal())
+    embed.add_field(
+        name="1️⃣ | Création",
+        value="Permet de débuter la création d'un Persona", 
+        inline=False,
+    )
+    embed.add_field(
+        name="2️⃣ | Édition",
+        value="Permet d'éditer un Persona", 
+        inline=False,
+    )
+    embed.add_field(
+        name="3️⃣ | Suppression",
+        value="Permet de supprimer un Persona",
+        inline=False
+    )
+    embed.set_footer(
+        text="Cliquez sur la réaction pour choisir !\n ❌ Pour quitter le menu."
+    )
+    q= await ctx.send(embed=embed)
+    i= 0
+    while emoji[i] !="✅":
+        await q.add_reaction(emoji[i])
+        i+=1
+    reaction, user = await bot.wait_for("reaction_add", timeout=300, check=checkValid)
+    if reaction.emoji == "1️⃣": #Création
+        await q.delete()
+        await webhook_create(ctx, bot)
+        return
+    elif reaction.emoji == "2️⃣": #Edition
+        await q.delete()
+        await menu_edit(ctx, bot)
+        return
+        
+    elif reaction.emoji == "3️⃣": #Deletion
+        await q.delete()
+        await gestion.webhook_delete(ctx, bot)
+        return
+    else:
+        await ctx.send("Annulation !")
         return
