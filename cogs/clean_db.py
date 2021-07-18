474a1a9da3445a9bf97875433a536e609eae6156
mara-li <mara-li@outlook.fr>
2021-07-18 22:03:22 +0200

    Add more DB clean

--- cogs/clean_db.py
+++ cogs/clean_db.py
@@ -10,6 +10,7 @@ intents = discord.Intents(messages=True, guilds=True, reactions=True, members=Tr
 class DB_utils(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.bot.loop.create_task(self.update_DB())
 
     @commands.command(hidden=True)
     @commands.has_permissions(administrator=True)
@@ -92,6 +93,133 @@ class DB_utils(commands.Cog):
                 + ""
             )
             c.execute(sql)
+
+
+async def update_DB_Connect(self):
+    await self.bot.wait_until_ready()
+    db = sqlite3.connect("src/owlly.db", timeout=3000)
+    c = db.cursor()
+    server = (
+        "INSERT OR IGNORE INTO SERVEUR(prefix, idS, roliste, notes, "
+        "rolerm, chanRP, maxDC, sticky, tag, tokenHRP, delete_hrp, delay_hrp) VALUES (?,?,?,?,?,?,?,?,?,?,?,?)"
+    )
+    fiche = (
+        "INSERT OR IGNORE INTO FICHE(ids, fiche_pj, fiche_validation, "
+        "champ_general, champ_physique) VALUES(?,?,?, ?,?)"
+    )
+    guilds_list = self.bot.guilds
+    for i.id in guilds_list:
+        servvar = ("?", i, "0", 0, "0", "0", 0, 0, "0", "0", 0, 0)
+        fichevar = (i, 0, 0, 0, "0", "0")
+        c.execute(server, servvar)
+        c.execute(fiche, fichevar)
+    db.commit()
+    c.close()
+    db.close()
+
+    @commands.Cog.listener()
+    async def on_guild_channel_delete(channel):
+        db = sqlite3.connect("src/owlly.db", timeout=3000)
+        c = db.cursor()
+        delete = channel.id
+        sql = "SELECT created_by FROM AUTHOR WHERE created_by =?"
+        c.execute(sql, (delete,))
+        verif_ticket = c.fetchone()
+        if verif_ticket is not None:
+            sql = "SELECT num FROM TICKET WHERE idM = ?"
+            c.execute(sql, verif_ticket)
+            count = c.fetchone()
+            if count > 0:
+                count = int(count[0]) - 1
+            else:
+                count = 0
+            sql = "UPDATE TICKET SET num = ? WHERE idM = ?"
+            var = (count, (int(verif_ticket[0])))
+            c.execute(sql, var)
+        sql = "DELETE FROM AUTHOR WHERE channel_id = ?"
+        c.execute(sql, (delete,))
+        sql = "DELETE FROM TICKET WHERE channelM = ?"
+        c.execute(sql, (delete,))
+        sql = "DELETE FROM CATEGORY WHERE channelM = ?"
+        c.execute(sql, (delete,))
+        db.commit()
+        c.close()
+        db.close()
+
+    @commands.Cog.listener()
+    async def on_member_remove(member):
+        dep = int(member.id)
+        idS = int(member.guild.id)
+        db = sqlite3.connect("src/owlly.db", timeout=3000)
+        c = db.cursor()
+        sql = "DELETE FROM AUTHOR WHERE (UserID = ? AND idS = ?)"
+        c.execute(
+            sql,
+            (
+                dep,
+                idS,
+            ),
+        )
+        sql = "DELETE FROM DC WHERE (idS = ? AND idU=?)"
+        c.execute(
+            sql,
+            (
+                idS,
+                dep,
+            ),
+        )
+        db.commit()
+        c.close()
+        db.close()
+
+    @commands.Cog.listener()
+    async def on_raw_message_delete(payload):
+        mid = payload.message_id
+        serv = payload.guild_id
+        db = sqlite3.connect("src/owlly.db", timeout=3000)
+        c = db.cursor()
+        sql = "SELECT idM FROM TICKET WHERE idS=?"
+        c.execute(sql, (serv,))
+        ticket_list = c.fetchall()
+        ticket_list = list(sum(ticket_list, ()))
+        sql = "SELECT idM FROM CATEGORY WHERE idS = ?"
+        c.execute(sql, (serv,))
+        cat_list = c.fetchall()
+        cat_list = list(sum(cat_list, ()))
+
+        for i in ticket_list:
+            if i == mid:
+                sql = "DELETE FROM TICKET WHERE idS=?"
+                c.execute(sql, (serv,))
+        for i in cat_list:
+            if i == mid:
+                sql = "DELETE FROM CATEGORY WHERE idS = ?"
+                c.execute(sql, (serv,))
+
+        db.commit()
+        c.close()
+        db.close()
+
+    @command.Cog.listener()
+    async def on_guild_remove(guild):
+        server = guild.id
+        db = sqlite3.connect("src/owlly.db", timeout=3000)
+        c = db.cursor()
+        sql1 = "DELETE FROM AUTHOR WHERE idS = ?"
+        sql2 = "DELETE FROM TICKET WHERE idS = ?"
+        sql3 = "DELETE FROM CATEGORY WHERE idS = ?"
+        sql4 = "DELETE FROM FICHE WHERE idS = ?"
+        sql5 = "DELETE FROM DC WHERE idS = ?"
+        c.execute(sql1, (server,))
+        c.execute(sql2, (server,))
+        c.execute(sql3, (server,))
+        c.execute(sql4, (server,))
+        c.execute(sql5, (server,))
+        sql = "DELETE FROM SERVEUR WHERE idS = ?"
+        c.execute(sql, (server,))
+        db.commit()
+        c.close()
+        db.close()
 
 
 def setup(bot):
